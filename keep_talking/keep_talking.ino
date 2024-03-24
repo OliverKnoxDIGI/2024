4dff849f707cb21b32cae88d5364b35772749ef0
OliverKnoxDIGI <19308@orewacollege.nz>
2024-03-25 10:01:47 +1300

    Update keep_talking.ino

--- keep_talking/keep_talking.ino
+++ keep_talking/keep_talking.ino
@@ -6,44 +6,99 @@
 const int wire_1 = 1;
 const int wire_2 = 2;
 const int wire_3 = 3;
-const int CORRECTWIRE = 3;
 
-int wire_cut = 0;
+const int button1 = 4;
+const int button2 = 5;
+const int button3 = 6;
+const int button4 = 7;
+const int button5 = 8;
+
+boolean button1_pressed = false;
+boolean button2_pressed = false;
+boolean button3_pressed = false;
+boolean button4_pressed = false;
+boolean button5_pressed = false;
+
 
 void setup() {
   // put your setup code here, to run once:
- Serial.begin(9600);
- pinMode(wire_1, INPUT);
- pinMode(wire_2, INPUT);
- pinMode(wire_3, INPUT);
+  Serial.begin(9600);
+  pinMode(wire_1, INPUT);
+  pinMode(wire_2, INPUT);
+  pinMode(wire_3, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-cut_wires();
+  boolean val = cut_wires();
+  if (cut_wires() == true) {
+    boolean buttons();
+  }
 }
 
-void cut_wires() {
- //3 wires
+boolean cut_wires() {
+  //3 wires
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (digitalRead(wire_1) == LOW) {
-    
+
     printf("task failed");
+    return false;
   }
-  
+
   if (digitalRead(wire_2) == LOW)  {
 
     printf("task failed");
- 
+    return false;
+
   }
 
-    if (digitalRead(wire_3) == LOW)  {
+  if (digitalRead(wire_3) == LOW)  {
 
     printf("task complete");
- 
+    return true;
+
   }
 }
-void buttons() {
+boolean buttons() {
+
+  //5 buttons pressed in correct sequence
+
+  if (digitalRead(button1) == HIGH) {
+    button1_pressed = true;
+    printf("correct button pressed");
+  }
   
+  if (digitalRead(button2) == HIGH && button1_pressed == true) {
+    button2_pressed = true;
+      printf("correct button pressed");
+  } else if (digitalRead(button2) == HIGH && button1_pressed == false) {
+    printf("task failed");
+  }
+  
+  if (digitalRead(button3) == HIGH && button2_pressed == true) {
+    button3_pressed = true;
+      printf("correct button pressed");
+  }
+   else if (digitalRead(button3) == HIGH && button2_pressed == false) {
+    printf("task failed");
+  }
+  
+  if (digitalRead(button4) == HIGH && button3_pressed == true) {
+    button4_pressed = true;
+      printf("correct button pressed");
+  }
+   else if (digitalRead(button4) == HIGH && button3_pressed == false) {
+    printf("task failed");
+  }
+  
+  if (digitalRead(button5) == HIGH && button4_pressed == true) {
+    button5_pressed = true;
+      printf("correct button pressed");
+  }
+   else if (digitalRead(button5) == HIGH && button4_pressed == false) {
+    printf("task failed");
+  }
+
+
 }
