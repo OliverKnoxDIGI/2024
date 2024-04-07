641d930684a09377d94334ce17d5398385710094
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2024-04-07 14:51:43 +1200

    Update keep_talking.ino
    teacher notes added

--- keep_talking/keep_talking.ino
+++ keep_talking/keep_talking.ino
@@ -3,27 +3,29 @@
 
 #include <Wire.h> 
 
-
+//TEACHER COMMENTS what is HELPER_ADDR maybe a better name is needed
 const byte HELPER_ADDR = 1;
+//TEACHER COMMENTS do you use a potentiometer in this example at all?
 const byte POT = A0;
 
+//TEACHER COMMENTS what is this variable used for?
 int alive = 1;
 
-
+//TEACHER COMMENTS what are these for?
 const int wire_1 = 1;
 const int wire_2 = 2;
 const int wire_3 = 3;
 const int failLed = 9;
 const int passedLed = 10;
 
-
+//TEACHER COMMENTS what about these?
 const int button1 = 4;
 const int button2 = 5;
 const int button3 = 6;
 const int button4 = 7;
 const int button5 = 8;
 
-
+//TEACHER COMMENTS what are these for?
 boolean button1_pressed = false;
 boolean button2_pressed = false;
 boolean button3_pressed = false;
@@ -34,7 +36,7 @@ boolean button5_pressed = false;
 void setup() {
   // put your setup code here, to run once:
     Wire.begin(HELPER_ADDR);
-   pinMode(POT, INPUT);
+   pinMode(POT, INPUT); //TEACHER COMMENTS do you need this?
    Wire.onRequest(requestEvent);
   Serial.begin(9600);
   pinMode(wire_1, INPUT);
@@ -47,18 +49,16 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //TEACHER COMMENTS what is this chunk of code doing?
   boolean val = cut_wires();
-  if (cut_wires() == true) {
+  if (cut_wires() == true) { //TEACHER COMMENTS do you mean val? or do you run cut_wires a second time?
   //send event won
-    Wire.write(alive = 1);
-    buttons();
-  }
-  else {
+  //  Wire.write(alive = 1); //TEACHER COMMENTS i have comment this out and added the line below, is that what you want? you have tried to do the requestEvent method in the middle of the loop it looks like?
+    alive = 1;
+    buttons(); //TEACHER COMMENTS does this mean you have to do the cut wires module first? why not separate these out into two 
+  } else {
    // send event lost
      Wire.write(alive = 0);
-    
-    
   }
 }
 
@@ -91,6 +91,9 @@ boolean cut_wires() {
 
   }
 }
+
+//TEACHER COMMENTS add the correct button sequence as a comment
+//TEACHER COMMENTS also explain where the return statement has disappeared to?
 boolean buttons() {
 
   //5 buttons pressed in correct sequence
@@ -133,6 +136,7 @@ boolean buttons() {
 
 }
 
+//TEACHER COMMENTS so what is this method for and what potential values are expected to be sent?
 void requestEvent(){
   //reply to the transmission request with this message
   Wire.write(alive);
