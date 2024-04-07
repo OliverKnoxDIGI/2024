726236cbfc614145e44d231ee8f4942f4c035c21
OliverKnoxDIGI <19308@orewacollege.nz>
2024-04-08 10:10:05 +1200

    Update keep_talking.ino

--- keep_talking/keep_talking.ino
+++ keep_talking/keep_talking.ino
@@ -5,8 +5,7 @@
 
 //TEACHER COMMENTS what is HELPER_ADDR maybe a better name is needed
 const byte HELPER_ADDR = 1;
-//TEACHER COMMENTS do you use a potentiometer in this example at all?
-const byte POT = A0;
+
 
 //TEACHER COMMENTS what is this variable used for?
 int alive = 1;
@@ -36,7 +35,6 @@ boolean button5_pressed = false;
 void setup() {
   // put your setup code here, to run once:
     Wire.begin(HELPER_ADDR);
-   pinMode(POT, INPUT); //TEACHER COMMENTS do you need this?
    Wire.onRequest(requestEvent);
   Serial.begin(9600);
   pinMode(wire_1, INPUT);
@@ -51,14 +49,20 @@ void setup() {
 void loop() {
   //TEACHER COMMENTS what is this chunk of code doing?
   boolean val = cut_wires();
-  if (cut_wires() == true) { //TEACHER COMMENTS do you mean val? or do you run cut_wires a second time?
+  if (val == true) {
   //send event won
-  //  Wire.write(alive = 1); //TEACHER COMMENTS i have comment this out and added the line below, is that what you want? you have tried to do the requestEvent method in the middle of the loop it looks like?
     alive = 1;
     buttons(); //TEACHER COMMENTS does this mean you have to do the cut wires module first? why not separate these out into two 
   } else {
-   // send event lost
-     Wire.write(alive = 0);
+   //  event lost
+     alive = 0;
+  }
+  boolean val2 = buttons();
+  if (val2 == true) {
+     Serial.println("game won");
+  } else{
+    alive = 0;
+    Serial.println("game Lost");
   }
 }
 
@@ -92,7 +96,7 @@ boolean cut_wires() {
   }
 }
 
-//TEACHER COMMENTS add the correct button sequence as a comment
+//button seq = 1,2,3,4,5
 //TEACHER COMMENTS also explain where the return statement has disappeared to?
 boolean buttons() {
 
@@ -108,6 +112,7 @@ boolean buttons() {
     Serial.println("correct button pressed");
   } else if (digitalRead(button2) == HIGH && button1_pressed == false) {
     Serial.println("task failed");
+    return false;
   }
 
   if (digitalRead(button3) == HIGH && button2_pressed == true) {
@@ -116,6 +121,7 @@ boolean buttons() {
   }
   else if (digitalRead(button3) == HIGH && button2_pressed == false) {
     Serial.println("task failed");
+    return false;
   }
 
   if (digitalRead(button4) == HIGH && button3_pressed == true) {
@@ -124,14 +130,17 @@ boolean buttons() {
   }
   else if (digitalRead(button4) == HIGH && button3_pressed == false) {
     Serial.println("task failed");
+    return false;
   }
 
   if (digitalRead(button5) == HIGH && button4_pressed == true) {
     button5_pressed = true;
     Serial.println("correct button pressed");
+    return true;
   }
   else if (digitalRead(button5) == HIGH && button4_pressed == false) {
     Serial.println("task failed");
+    return false;
   }
 
 }
